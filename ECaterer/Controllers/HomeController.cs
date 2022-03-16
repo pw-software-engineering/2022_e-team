cc3c8ffa767a49a1d3d64b41ac6b854959a0accf
H4kan <szymon.sieradzki184@wp.pl>
2022-03-16 18:56:41 +0100

    initial web project

---
+++ ECaterer/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ECaterer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Get()
+        {
+            return Ok("Hello!");
+        }
+    }
+}
