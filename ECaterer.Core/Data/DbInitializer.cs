0bb5f2d13b0215327cb15b6e4d0d27eda7d7c51e
H4kan <szymon.sieradzki184@wp.pl>
2022-03-16 20:25:35 +0100

    moved web project out of solution

---
+++ ECaterer.Core/Data/DbInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECaterer.Core.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(DataContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
