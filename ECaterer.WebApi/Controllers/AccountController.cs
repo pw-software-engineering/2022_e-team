0fd5d74912f22636749c068a684844bcacca8921
Piotr Sawicki <01151556@pw.edu.pl>
2022-04-03 22:34:48 +0200

    Add basic Account Controller

---
+++ ECaterer.WebApi/Controllers/AccountController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECaterer.WebApi.Models;
+using ECaterer.WebApi.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECaterer.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly TokenService _tokenService;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, TokenService tokenService)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _tokenService = tokenService;
+        }
+
+        [Route("Login")]
+        [HttpPost]
+        public async Task<ActionResult<AuthenticatedUserModel>> Login([FromBody] LoginUserModel loginUser)
+        {
+            var user = await _userManager.FindByEmailAsync(loginUser.Email);
+
+            if (user == null)
+                return Unauthorized();
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginUser.Password, false);
+
+            if (result.Succeeded)
+            {
+                return new AuthenticatedUserModel
+                {
+                    Token = _tokenService.CreateToken(user),
+                   // UserName = user.Email
+                };
+            }
+
+            return Unauthorized();
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<AuthenticatedUserModel>> Register(RegisterUserModel registerUser)
+        {
+            if (await _userManager.Users.AnyAsync(x => x.Email == registerUser.Email))
+            {
+                return BadRequest("Email taken");
+            }
+
+            var user = new IdentityUser
+            {
+                Email = registerUser.Email,
+                UserName = registerUser.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, registerUser.Password);
+
+            if (result.Succeeded)
+            {
+                return new AuthenticatedUserModel
+                {
+                    //UserName = user.UserName,
+                    Token = _tokenService.CreateToken(user)
+                };
+
+            }
+
+            return BadRequest("Problem registering user");
+        }
+    }
+}
--- ECaterer.WebApi/Startup.cs
+++ ECaterer.WebApi/Startup.cs
@@ -1,4 +1,7 @@
+using ECaterer.Core;
 using ECaterer.WebApi.Data;
+using ECaterer.WebApi.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -8,9 +11,12 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ECaterer.WebApi
@@ -27,14 +33,90 @@ namespace ECaterer.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(policy =>
+            {
+                policy.AddPolicy("OpenCorsPolicy", opt =>
+                    opt.AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod());
+            });
+
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDatabaseDeveloperPageExceptionFilter();
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddRazorPages();
-            services.AddSwaggerGen();
+
+            services.AddControllers();
+
+            //services.AddControllersWithViews();
+            //services.AddRazorPages();
+
+            //services.AddIdentityServices(Configuration);
+
+            //IDENTITY
+            services.AddIdentityCore<IdentityUser>(opt =>
+            {
+                opt.Password.RequireNonAlphanumeric = false;
+            })
+                .AddEntityFrameworkStores<DataContext>()
+                .AddSignInManager<SignInManager<IdentityUser>>();
+
+            //TODO
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("DodajDoConfigaSecurityKey"));
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(opt =>
+                {
+                    opt.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = key,
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ClockSkew = TimeSpan.FromMinutes(5)
+                    };
+
+                });
+
+            services.AddScoped<TokenService>();
+
+            //SWAGGER
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "My API",
+                    Version = "v1"
+                });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please insert JWT with Bearer into field",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+               {
+                 new OpenApiSecurityScheme
+                 {
+                   Reference = new OpenApiReference
+                   {
+                     Type = ReferenceType.SecurityScheme,
+                     Id = "Bearer"
+                   }
+                  },
+                  new string[] { }
+                }
+              });
+            })
+                ;
+          
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,17 +125,18 @@ namespace ECaterer.WebApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseMigrationsEndPoint();
+                //app.UseDatabaseErrorPage();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi v1"));
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
+            //else
+            //{
+            //    app.UseExceptionHandler("/Home/Error");
+            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //    app.UseHsts();
+            //}
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            app.UseCors("OpenCorsPolicy");
+           // app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -62,13 +145,7 @@ namespace ECaterer.WebApi
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
-            });
-
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Test1 Api v1");
+                endpoints.MapControllers();
             });
         }
     }
