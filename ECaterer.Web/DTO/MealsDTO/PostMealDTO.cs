6c8678acbbdf807ccf7330e486aeb00b20bf677b
Maksim Makaranka <maksim.makaranka@gmail.com>
2022-04-10 19:03:27 +0200

    Added controller methods signatures and needed DTO objects

---
+++ ECaterer.Web/DTO/MealsDTO/PostMealDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECaterer.Web.DTO.MealsDTO
+{
+    public class PostMealDTO
+    {
+        public string Name { get; set; }
+        public string[] AllergenList { get; set; }
+        public string[] IngredientList { get; set; }
+        public int Calories { get; set; }
+        public bool Vegan { get; set; }
+    }
+}
---
+++ ECaterer.WebApi/Controllers/MealsController.cs
@@ -0,0 +1,56 @@
+﻿using ECaterer.Core;
+using ECaterer.Core.Models;
+using ECaterer.Web.DTO.MealsDTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECaterer.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class MealsController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public MealsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public Task<ActionResult<GetMealDTO[]>> GetMeals(int offset, int limit, string sort, string name, string name_with, bool vegan, int colories, int colories_lt, int colories_ht)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public Task<ActionResult> PostMeal([FromBody] PostMealDTO mealDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{mealId}")]
+        public Task<ActionResult<Meal>> GetMealById(string mealId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{mealId}")]
+        public Task<ActionResult<Meal>> EditMeal(string mealId, [FromBody] Meal meal)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{mealId}")]
+        public Task<ActionResult> DeleteMeal(string mealId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
