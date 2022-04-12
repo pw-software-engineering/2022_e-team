b37f37246ba84d1ada3f66653fe3d2ad95a27618
Maksim Makaranka <maksim.makaranka@gmail.com>
2022-04-12 15:43:15 +0200

    Added test file

---
+++ ECareter.Web.Test/ApiUnitTests/MealsTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using ECaterer.Core;
+using FluentAssertions;
+using Xunit;
+using Microsoft.EntityFrameworkCore;
+using ECaterer.Core.Models;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ECaterer.Web.DTO.MealsDTO;
+using Moq;
+
+namespace ECareter.Web.Test.ApiUnitTests
+{
+    public class MealsTests
+    {
+        [Fact]
+        public void DeleteMeal_ShouldDeleteMealAndReturnOk()
+        {
+            var contextMock = new Mock<DataContext>();
+        }
+    }
+}
