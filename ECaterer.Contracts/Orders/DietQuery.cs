aa9f0e5f3b7a7c0e58260e3641d2ca5fb0e7ce62
Bartek <michalski235@gmail.com>
2022-04-27 02:03:48 +0200

    Minor change

---
+++ ECaterer.Contracts/Orders/DietQuery.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECaterer.WebApi.Common.Queries
+{
+    public class DietQuery
+    {
+        public int? Offset { get; set; }
+        public int? Limit { get; set; }
+        public string Sort { get; set; }
+        public string Name { get; set; }
+        public string NameWith { get; set; }
+        public bool? Vegan { get; set; }
+        public int? Calories { get; set; }
+        public int? CaloriesLt { get; set; }
+        public int? CaloriesHt { get; set; }
+        public int? Price { get; set; }
+        public int? PriceLt { get; set; }
+        public int? PriceHt { get; set; }
+    }
+}
