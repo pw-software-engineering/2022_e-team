dc8e9e944f79ab04e4a74c2bc120cc21997e1b5b
Bartek <michalski235@gmail.com>
2022-06-15 02:33:22 +0200

    Integrate client controller

---
+++ ECaterer.Web/Converters/OrderConverter.cs
@@ -0,0 +1,25 @@
+﻿using ECaterer.Contracts.Orders;
+using ECaterer.Web.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECaterer.Web.Converters
+{
+    public static class OrderConverter
+    {
+        public static ClientOrdersDTO ConvertBack(OrderClientModel input)
+        {
+            return new ClientOrdersDTO()
+            {
+                OrderNumber = "1",
+                StartDate = input.StartDate,
+                EndDate = input.EndDate,
+                Status = input.Status,
+                Price = input.Price,
+                ComplaintStatus = input.Complaint.Status
+            };
+        }
+    }
+}
